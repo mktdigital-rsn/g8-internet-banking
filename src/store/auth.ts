dff84f20ac1bb64d77d42977a83704ed63f54601
Roberto <robertoguedesn@gmail.com>
2026-04-15 12:06:58 -0300

    Make temporary device id storage SSR-safe

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,6 +1,23 @@
 import { atomWithStorage, createJSONStorage } from "jotai/utils";
 
-const temporaryDeviceIdStorage = createJSONStorage<string>(() => localStorage);
+const memoryStorage: Storage = {
+  length: 0,
+  clear: () => undefined,
+  getItem: () => null,
+  key: () => null,
+  removeItem: () => undefined,
+  setItem: () => undefined,
+};
+
+const getStorage = () => {
+  if (typeof window === "undefined") {
+    return memoryStorage;
+  }
+
+  return window.localStorage;
+};
+
+const temporaryDeviceIdStorage = createJSONStorage<string>(getStorage);
 
 export const temporaryDeviceIdAtom = atomWithStorage(
   "temporaryDeviceId",
